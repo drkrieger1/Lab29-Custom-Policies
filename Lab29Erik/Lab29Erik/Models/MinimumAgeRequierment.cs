f12eed06606ec84b242a59d326735a8cd01e050c
drkrieger <eplyushko@gmail.com>
2017-10-25 05:41:54 -0700

    min age added

---
+++ Lab29Erik/Lab29Erik/Models/MinimumAgeRequierment.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab29Erik.Models
+{
+    public class MinimumAgeRequierment : IAuthorizationRequirement
+    {
+    }
+}
